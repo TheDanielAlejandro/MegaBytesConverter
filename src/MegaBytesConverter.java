6662b51d6e5ab25607ec3a732ee0176bc63f0eb3
Daniel Alejandro Lopez Munera <danielalejandrolopez97@gmail.com>
2020-08-11 11:31:15 +0200

    MegaBytesConverter.

---
+++ src/MegaBytesConverter.java
@@ -0,0 +1,13 @@
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int megaBytes = (int) Math.floor(kiloBytes / 1024);
+            int rest = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + rest + " KB");
+        }
+    }
+
+}
